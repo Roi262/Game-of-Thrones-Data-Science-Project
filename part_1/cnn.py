3350c56e55a686ebc21d3361f95f4d5d37ea51e0
Ori Matityahu <ori.matityahou@gmail.com>
2020-03-07 22:32:30 +0200

    more code cleaning

--- part_1/cnn.py
+++ part_1/cnn.py
@@ -73,24 +73,6 @@ def train_model(model, x_train, y_train, x_test, y_test):
 
 
 def plot_history(history):
-    # acc = history.history['accuracy']
-    # val_acc = history.history['val_accuracy']
-    # loss = history.history['loss']
-    # val_loss = history.history['val_loss']
-    # x = range(1, len(acc) + 1)
-    #
-    # plt.figure(figsize=(12, 5))
-    # plt.subplot(1, 2, 1)
-    # plt.plot(x, acc, 'b', label='Training accuracy')
-    # plt.plot(x, val_acc, 'r', label='Validation accuracy')
-    # plt.title('Training and validation accuracy')
-    # plt.legend()
-    # plt.subplot(1, 2, 2)
-    # plt.plot(x, loss, 'b', label='Training loss')
-    # plt.plot(x, val_loss, 'r', label='Validation loss')
-    # plt.title('Training and validation loss')
-    # plt.legend()
-    # plt.show()
 
     accuracy = history.history['accuracy']
     validation_accuracy = history.history['val_accuracy']
